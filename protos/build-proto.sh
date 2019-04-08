7c3be60f2d97c44a3af2c76da4c50024b5e5a704
Thibaut <thbtdwt@protonmail.com>
2019-04-08 21:53:53 +0200

    fix path in build_proto.sh

--- protos/build-proto.sh
+++ protos/build-proto.sh
@@ -2,6 +2,6 @@
 
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 protos_dir=${script_dir}/
-python_dir=${script_dir}/../smbus_rpc
+python_dir=${script_dir}/../smbus_proxy
 
 python -m grpc_tools.protoc -I${protos_dir} --python_out=${python_dir} --grpc_python_out=${python_dir} ${protos_dir}/smbusRpc.proto
--- smbus_proxy/build-proto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-script_dir=$(dirname "${BASH_SOURCE[0]}")
-protos_dir=${script_dir}/protos
-
-
-python -m grpc_tools.protoc -I${protos_dir} --python_out=${script_dir}/ --grpc_python_out=${script_dir} ${protos_dir}/smbusRpc.proto
